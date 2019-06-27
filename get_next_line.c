db3003da708e4b58b7038cc08b0158ac0eb92858
Lindo NKAMBULE <lnkambul@c4r16s2.wethinkcode.co.za>
2019-06-27 18:28:11 +0200

    progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,47 @@
 /*   By: lnkambul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 15:07:28 by lnkambul          #+#    #+#             */
-/*   Updated: 2019/06/26 17:16:55 by lnkambul         ###   ########.fr       */
+/*   Updated: 2019/06/27 18:27:35 by lnkambul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char		*bank;
+static char		**buffer;
 
 int				get_next_line(const int fd, char **line)
 {
-	
+	char		*bank;
+	char		*temp;
+	size_t		i;
+
+	buffer = (char**)malloc(sizeof(char *) * 1023);
+	bank = ft_strnew(BUFF_SIZE);
+	i = 2;
+	while (read(fd, bank, BUFF_SIZE) > 0)
+	{
+		if(!ft_strchr(bank, '\n'));
+		{
+			temp = ft_strdup(bank);
+			ft_strdel(&bank);
+			free(bank);
+			bank = ft_strnew(BUFF_SIZE * i++);
+		}
+
+		ft_strdel(line);
+	}
+	if (!buffer[fd])
+		buffer = ft_strnew(
 }
+
+/*
+int				main ()
+{
+	char	*fn;
+
+	fn = ft_strdup("f1");
+
+	ft_putendl(get_next_line(, &bank);
+	return (0);
+}
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: lnkambul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 15:12:57 by lnkambul          #+#    #+#             */
-/*   Updated: 2019/06/26 15:29:38 by lnkambul         ###   ########.fr       */
+/*   Updated: 2019/06/27 12:51:52 by lnkambul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
-#define BUFF 32
+#define BUFF_SIZE 32
 #include "libft.h"
 
 int		get_next_line(const int fd, char **line);
