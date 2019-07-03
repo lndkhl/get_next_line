292bada405195674f2512ae9164d7fa8a583832b
Lindo NKAMBULE <lnkambul@c4r16s2.wethinkcode.co.za>
2019-07-03 15:20:28 +0200

    updated to first ray of light

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: lnkambul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 15:07:28 by lnkambul          #+#    #+#             */
-/*   Updated: 2019/07/03 13:54:45 by lnkambul         ###   ########.fr       */
+/*   Updated: 2019/07/03 15:20:09 by lnkambul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include " get_next_line.h "
 #include <fcntl.h>
-
+/*
 t_lines			*ft_lnsnew(int fd, char *l, char *b)
 {
 	t_lines		newline;
@@ -40,22 +40,26 @@ t_lines			*ft_lnsdelone(int fd, t_lines **aln)
 	(*aln)->line = NULL;
 	ft_memdel((void **)aln);
 }
-
+*/
 int				get_next_line(const int fd, char **line)
 {
-	static t_lines	**lines;
-	t_lines			*node;
+	//static t_lines	**lines;
+	static char		**arr;
+	//t_lines		*node;
 	char			*bank;
 	char			*temp;
-
+	/*
 	if (!(lines = (t_lines **)malloc(sizeof(t_lines) * 1024)))
+		return (-1);
+	*/
+	if (!(arr = (char **)malloc(sizeof(char *) * 1024)))
 		return (-1);
 	if (fd < 0 || !line || read(fd, NULL, 0) == -1)
 		return (-1);
 	bank = ft_strnew(BUFF_SIZE);
 	while (read(fd, bank, BUFF_SIZE) > 0)
 	{
-		if(ft_strchr(bank, '\n'))
+		if(!(ft_strchr(bank, '\n')))
 		{
 			temp = (ft_strlen(temp)) ? ft_strjoin(temp, bank) : ft_strdup(bank);
 			ft_strdel(&bank);
@@ -64,23 +68,14 @@ int				get_next_line(const int fd, char **line)
 		}
 		else
 		{
-			while (ft_strchr(bank, '\n'))
-			{
-				while (*bank)
-				{
-					temp = ft_strsub(bank, 0, ft_strchr(bank, '\n') - bank);
-					bank = ft_strchr(bank, '\n') + 1;
-					node = ft_lnsnew(fd, temp, bank);
-				if (!(lines[fd]))
-					lines[fd] = node;
-				else
-					ft_lnsadd(&lines, node);
-			}
-			*line = (*buffer)->content;
-			ft_lstdelone(buffer, ft_strdel(buffer), (*buffer)->content);
+			*line = ft_strsub(bank, 0, size_t(ft_strchr(bank, '\n') - bank));
+			temp = ft_strchr(bank, '\n') + 1;
+			//node = ft_lnsnew(fd, temp, bank);
+			arr[fd] = ft_strdup(temp);
+			return (0);
 		}
 	}
-	return (1);
+	return (0);
 }
 
 #include <stdio.h>
@@ -93,7 +88,6 @@ int			main()
 
 	fd = open( "", O_RDONLY);
 	output = 1;
-
 	while (output > 0)
 	{
 		output = get_next_line(fd, &line);
