b0b22373033114dd8fb93d33514918611319e150
Lindo NKAMBULE <lnkambul@c4r16s2.wethinkcode.co.za>
2019-07-08 17:05:45 +0200

    updated gnl to 1 memleak version

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,20 @@
 /*   By: lnkambul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 16:20:06 by lnkambul          #+#    #+#             */
-/*   Updated: 2019/07/06 21:07:01 by lnkambul         ###   ########.fr       */
+/*   Updated: 2019/07/08 16:34:42 by lnkambul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+
 int					get_next_line(const int fd, char **line)
 {	
 	static char		*arr[1024];
 	char			*temp;
+	int				i;
 
-	if (fd < 0 || !line || read(fd, NULL, 0) == -1)
+if (fd < 0 || !line || read(fd, NULL, 0) == -1)
 		return (-1);
 	if (!(arr[fd]) && !(arr[fd] = ft_strnew(BUFF_SIZE)))
 			return (-1);
@@ -33,13 +35,14 @@ int					get_next_line(const int fd, char **line)
 		return (1);
 	}
 	temp = ft_strnew(BUFF_SIZE);
-	while (read(fd, temp, BUFF_SIZE))
+	if ((i = read(fd, temp, BUFF_SIZE)))
 	{
 		arr[fd] = (arr[fd]) ? ft_strjoin(arr[fd], temp) : ft_strdup(temp);
 		if (ft_strchr(arr[fd], '\n'))
 		{
 			*line = ft_strsub(arr[fd], 0, (size_t)(ft_strchr(arr[fd], '\n')\
 						- arr[fd]));
+			ft_strdel(&temp);
 			temp = ft_strsub(ft_strchr(arr[fd], '\n') + 1, 0,\
 						ft_strlen(arr[fd]) - ft_strlen(*line));
 			ft_strdel(&arr[fd]);
@@ -47,11 +50,20 @@ int					get_next_line(const int fd, char **line)
 			ft_strdel(&temp);
 			return (1);
 		}
-		ft_strdel(&temp);
-		temp = ft_strnew(BUFF_SIZE);
+		else if (i < BUFF_SIZE)
+		{
+			*line = ft_strdup(arr[fd]);
+			ft_strdel(&arr[fd]);
+			ft_strdel(&temp);
+			return (1);
+		}
 	}
-	*line = (arr[fd]) ? arr[fd] : *line;
-	if (arr[fd])
+	if (i == 0 && arr[fd][0]){
+		*line = ft_strdup(arr[fd]);
 		ft_strdel(&arr[fd]);
-	return (0);
+		ft_strdel(&temp);
+		return (1);
+	}
+	ft_strdel(&temp);
+return (0);
 }
