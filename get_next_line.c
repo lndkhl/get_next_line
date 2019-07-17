b9f1196e42fb46a60fd0854062d18036aba1b11b
Lindo NKAMBULE <lnkambul@c4r16s2.wethinkcode.co.za>
2019-07-17 17:21:05 +0200

    new

--- get_next_line.c
+++ get_next_line.c
@@ -5,18 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lnkambul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/05 16:20:06 by lnkambul          #+#    #+#             */
-/*   Updated: 2019/07/10 13:50:54 by lnkambul         ###   ########.fr       */
+/*   Created: 2019/07/17 10:32:19 by lnkambul          #+#    #+#             */
+/*   Updated: 2019/07/17 17:20:46 by lnkambul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
 int					get_next_line(const int fd, char **line)
-{	
-	static char		*arr[1024];
+{
+	static char		*arr[4096];
 	char			*temp;
+	char			*t2;
 	int				i;
 
 if (fd < 0 || !line || read(fd, NULL, 0) == -1)
@@ -25,46 +25,58 @@ if (fd < 0 || !line || read(fd, NULL, 0) == -1)
 			return (-1);
 	if (ft_strchr(arr[fd], '\n'))
 	{
-		*line = ft_strsub(arr[fd], 0, (size_t)(ft_strchr(arr[fd], '\n')\
-						- arr[fd]));
-		temp = ft_strsub(ft_strchr(arr[fd], '\n') + 1, 0,\
-						ft_strlen(arr[fd]) - ft_strlen(*line));
+		if (!(*line = ft_strsub(arr[fd], 0, (ft_strchr(arr[fd], '\n') - arr[fd]))))
+			return (-1);
+		if (!(temp = ft_strsub(ft_strchr(arr[fd], '\n') + 1, 0,\
+						ft_strlen(arr[fd]) - ft_strlen(*line))))
+			return (-1);
 		ft_strdel(&arr[fd]);
 		arr[fd] = ft_strdup(temp);
 		ft_strdel(&temp);
 		return (1);
 	}
-	temp = ft_strnew(BUFF_SIZE);
+	if (!(temp = ft_strnew(BUFF_SIZE)))
+		return (-1);
 	if ((i = read(fd, temp, BUFF_SIZE)))
 	{
-		arr[fd] = (arr[fd]) ? ft_strjoin(arr[fd], temp) : ft_strdup(temp);
+		if (!(t2 = (arr[fd]) ? ft_strjoin(arr[fd], temp) : ft_strdup(temp)))
+			return (-1);
+		ft_strdel(&arr[fd]);
+		if (!(arr[fd] = ft_strdup(t2)))
+			return (-1);
+		ft_strdel(&t2);
 		if (ft_strchr(arr[fd], '\n'))
 		{
-			*line = ft_strsub(arr[fd], 0, (size_t)(ft_strchr(arr[fd], '\n')\
-						- arr[fd]));
+			if(!(*line = ft_strsub(arr[fd], 0, (ft_strchr(arr[fd], '\n') - arr[fd]))))
+				return (-1);
 			ft_strdel(&temp);
-			temp = ft_strsub(ft_strchr(arr[fd], '\n') + 1, 0,\
-						ft_strlen(arr[fd]) - ft_strlen(*line));
+			if(!(temp = ft_strsub(ft_strchr(arr[fd], '\n') + 1, 0,\
+						ft_strlen(arr[fd]) - ft_strlen(*line))))
+				return (-1);
 			ft_strdel(&arr[fd]);
-			arr[fd] = ft_strdup(temp);
+			if (!(arr[fd] = ft_strdup(temp)))
+				return (-1);
 			ft_strdel(&temp);
 			return (1);
 		}
 		else if (i < BUFF_SIZE)
 		{
-			*line = ft_strdup(arr[fd]);
+			if (!(*line = ft_strdup(arr[fd])))
+				return (-1);
 			ft_strdel(&arr[fd]);
 			ft_strdel(&temp);
 			return (1);
 		}
+		ft_strdel(&temp);
+		return (1);
 	}
-	if (i == 0 && arr[fd][0])
-	{
-		*line = ft_strdup(arr[fd]);
+	if (i == 0 && arr[fd][0]){
+		if (!(*line = ft_strdup(arr[fd])))
+			return (-1);
 		ft_strdel(&arr[fd]);
 		ft_strdel(&temp);
 		return (1);
 	}
 	ft_strdel(&temp);
-return (0);
+	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lnkambul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 12:28:25 by lnkambul          #+#    #+#             */
-/*   Updated: 2019/07/10 14:17:56 by lnkambul         ###   ########.fr       */
+/*   Updated: 2019/07/10 15:19:44 by lnkambul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ int			main(int argc, char **argv)
 	int		fd;
 	char	*line;
 
+	ft_putendl("line:");
 	fd = open(argv[1], O_RDONLY);
 	while (get_next_line(fd, &line) && argc < 3)
 	{
+		ft_putendl("line:");
 		ft_putendl(line);
 		ft_strdel(&line);
 	}
